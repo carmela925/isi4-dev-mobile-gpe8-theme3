b1e2f1d67b88f0d8c05b2f0fc2b1060f0a3b721a
Axel Limbaime <alimbaime8@gmail.com>
2025-01-10 00:13:18 +0100

    Echo

--- Front/CampusGuide/src/environments/environment.prod.ts
+++ Front/CampusGuide/src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
   logo: 'assets/img/logo-white-transparent.png',
+  firebaseConfig : {
+    apiKey: "AIzaSyAjJdvtveKnqayYyXaRUMtBWnvBrN3uV8I",
+    authDomain: "campus-guide-cavax.firebaseapp.com",
+    projectId: "campus-guide-cavax",
+    storageBucket: "campus-guide-cavax.firebasestorage.app",
+    messagingSenderId: "335295795748",
+    appId: "1:335295795748:web:019d3de2d35fd630b3feb1",
+    measurementId: "G-VVZ012NQSJ"
+  }
 };
